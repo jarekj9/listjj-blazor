5a755b7b21fc3519ba822b21eeddab7e74b347fa
jerry <jerry.esp.sha@gmail.com>
2022-03-27 19:50:54 +0200

    Add login link when logged out

--- deploy-azure.sh
+++ deploy-azure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rsync -avh Listjj/ machinejj:/home/jarek/docker/Listjj/Listjj --delete
+rsync -avh --exclude '*.env' Listjj/ machinejj:/home/jarek/docker/Listjj/Listjj --delete
 ansible machinejj -a 'docker rm -f listjj'
 ansible machinejj -a 'docker image rm listjj_blazor'
 ansible machinejj -a 'docker-compose -f /home/jarek/docker/Listjj/docker-compose.yml up -d'
