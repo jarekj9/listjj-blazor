6efa9251bcc05aff63cabdba765680d17b70f305
jerry <jerry.esp.sha@gmail.com>
2022-06-04 16:32:33 +0200

    deploy fixes

--- deploy-azure.sh
+++ deploy-azure.sh
@@ -2,5 +2,7 @@
 
 rsync -avh --exclude '*.env' Listjj/ machinejj:/home/jarek/docker/Listjj/Listjj --delete
 ansible machinejj -a 'docker rm -f listjj'
+ansible machinejj -a 'docker rm -f listjj_migrations_temp'
 ansible machinejj -a 'docker image rm listjj_blazor'
+ansible machinejj -a 'docker image rm listjj_migrations'
 ansible machinejj -a 'docker-compose -f /home/jarek/docker/Listjj/docker-compose.yml up -d'
