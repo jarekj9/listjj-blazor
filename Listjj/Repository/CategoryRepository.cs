ba15a3cdb7b627ff66780e9fff8e2d26050454d2
jerry <jerry.esp.sha@gmail.com>
2022-08-27 17:01:39 +0200

    cleanup

--- Listjj/Repository/CategoryRepository.cs
+++ Listjj/Repository/CategoryRepository.cs
@@ -5,7 +5,6 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Linq;
 using System;
 
 namespace Listjj.Repository
