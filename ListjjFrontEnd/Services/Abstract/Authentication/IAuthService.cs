342b7c0cd4a688bfca9062f4bb88b44a13e94961
Jaroslaw <jaroslaw.jankun@gmail.com>
2023-06-16 21:54:33 +0200

    Add empty blazor wasm project with basic register/login/logout using JWT and api.

---
+++ ListjjFrontEnd/Services/Abstract/Authentication/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using Listjj.Infrastructure.Data;
+using Listjj.Infrastructure.Models;
+
+namespace ListjjFrontEnd.Services.Abstract.Authentication
+{
+    public interface IAuthService
+    {
+        Task<RegisterResult> Register(RegisterModel registerModel);
+        Task<LoginResult> Login(LoginModel loginModel);
+        Task Logout();
+    }
+}
---
+++ ListjjFrontEnd/Services/Authentication/AuthService.cs
@@ -0,0 +1,60 @@
+﻿using Blazored.LocalStorage;
+using ListjjFrontEnd.Services.Abstract.Authentication;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text;
+using Listjj.Infrastructure.Models;
+using Listjj.Infrastructure.Data;
+using System.Net.Http.Json;
+
+namespace ListjjFrontEnd.Services.Authentication
+{
+    public class AuthService : IAuthService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly AuthenticationStateProvider _authenticationStateProvider;
+        private readonly ILocalStorageService _localStorage;
+
+        public AuthService(HttpClient httpClient,
+                           AuthenticationStateProvider authenticationStateProvider,
+                           ILocalStorageService localStorage)
+        {
+            _httpClient = httpClient;
+            _authenticationStateProvider = authenticationStateProvider;
+            _localStorage = localStorage;
+        }
+
+        public async Task<RegisterResult> Register(RegisterModel registerModel)
+        {
+            var response = await _httpClient.PostAsJsonAsync<RegisterModel>("http://localhost:5000/api/accounts", registerModel);
+            var result = await response.Content.ReadFromJsonAsync<RegisterResult>();
+            return result;
+        }
+
+        public async Task<LoginResult> Login(LoginModel loginModel)
+        {
+            var loginAsJson = JsonSerializer.Serialize(loginModel);
+            var response = await _httpClient.PostAsync("http://localhost:5000/api/Login", new StringContent(loginAsJson, Encoding.UTF8, "application/json"));
+            var loginResult = JsonSerializer.Deserialize<LoginResult>(await response.Content.ReadAsStringAsync(), new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return loginResult;
+            }
+
+            await _localStorage.SetItemAsync("authToken", loginResult.Token);
+            ((ApiAuthenticationStateProvider)_authenticationStateProvider).MarkUserAsAuthenticated(loginModel.Email);
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", loginResult.Token);
+
+            return loginResult;
+        }
+
+        public async Task Logout()
+        {
+            await _localStorage.RemoveItemAsync("authToken");
+            ((ApiAuthenticationStateProvider)_authenticationStateProvider).MarkUserAsLoggedOut();
+            _httpClient.DefaultRequestHeaders.Authorization = null;
+        }
+    }
+}
