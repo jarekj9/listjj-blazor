e890c1d900b7973f6798e7a0a21d987bd1cb7749
JJ <j-jan@outlook.com>
2026-04-01 17:58:50 +0200

    Add /api/version

---
+++ Listjj/APIs/VersionController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace Listjj.APIs
+{
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public VersionController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [Route("api/version")]
+        [HttpGet]
+        public async Task<IActionResult> GetVersion()
+        {
+            var version = new
+            {
+                version = _configuration.GetValue<string>("Version"),
+            };
+            return Ok(version);
+        }
+    }
+}
